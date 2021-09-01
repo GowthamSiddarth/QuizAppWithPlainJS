c0941e42ea00f837df4d5bde9469699632034728
Gowtham Behara <gothsiddu@gmail.com>
2021-09-01 18:11:43 +0530

    Add question number and score values

--- game.js
+++ game.js
@@ -3,7 +3,66 @@ const choicesTextElems = Array.from(
   document.getElementsByClassName("choice-text")
 );
 
-let acceptingAnswers = false;
+getNextQuestion = (availableQuestions) => {
+  let nextQuestionIdx = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestionIdx = nextQuestionIdx;
+  return availableQuestions[nextQuestionIdx];
+};
+
+setNextQuestionAndChoices = (nextQuestion) => {
+  questionEle.innerText = nextQuestion.question;
+  choicesTextElems.forEach((choice) => {
+    choice.innerText = nextQuestion["choice" + choice.dataset.number];
+  });
+  acceptingAnswers = true;
+};
+
+updateQuestionNumber = (questionNum) => {
+  const questionNumberEle = document.getElementById("questionNumber");
+  questionNumberEle.innerText = `${questionNum}/${MAX_QUESTIONS}`;
+};
+
+updateScore = (score) => {
+    const scoreEle = document.getElementById('score');
+    scoreEle.innerText = score;
+}
+
+playGame = (availableQuestions) => {
+  let currQuestionNum = MAX_QUESTIONS - availableQuestions.length + 1;
+  updateQuestionNumber(currQuestionNum);
+
+  let nextQuestion = getNextQuestion(availableQuestions);
+  currentQuestion = nextQuestion;
+  setNextQuestionAndChoices(nextQuestion);
+};
+
+choicesTextElems.forEach((choiceElem) => {
+  choiceElem.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    let selectionClass = "incorrect";
+    let selectedChoice = e.target;
+    if (parseInt(selectedChoice.dataset.number) === currentQuestion.answer) {
+      score++;
+      selectionClass = "correct";
+      updateScore(score);
+    }
+
+    selectedChoice.parentElement.classList.add(selectionClass);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(selectionClass);
+      availableQuestions.splice(currentQuestionIdx, 1);
+
+      if (availableQuestions.length) playGame(availableQuestions);
+      else {
+        alert("Game Over! Score = " + score);
+        window.location.replace("/");
+      }
+    }, 1000);
+  });
+});
+
 let questions = [
   {
     question: "Inside which HTML element do we put the JavaScript??",
@@ -32,54 +91,10 @@ let questions = [
   },
 ];
 
-getNextQuestion = (availableQuestions) => {
-  let nextQuestionIdx = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestionIdx = nextQuestionIdx;
-  return availableQuestions[nextQuestionIdx];
-};
-
-setNextQuestionAndChoices = (nextQuestion) => {
-  questionEle.innerText = nextQuestion.question;
-  choicesTextElems.forEach((choice) => {
-    choice.innerText = nextQuestion["choice" + choice.dataset.number];
-  });
-  acceptingAnswers = true;
-};
-
-playGame = (availableQuestions) => {
-  let nextQuestion = getNextQuestion(availableQuestions);
-  currentQuestion = nextQuestion;
-  setNextQuestionAndChoices(nextQuestion);
-};
-
-choicesTextElems.forEach((choiceElem) => {
-  choiceElem.addEventListener("click", (e) => {
-    if (!acceptingAnswers) return;
-
-    acceptingAnswers = false;
-    let selectionClass = "incorrect";
-    let selectedChoice = e.target;
-    if (parseInt(selectedChoice.dataset.number) === currentQuestion.answer) {
-      score++;
-      selectionClass = "correct";
-    }
-    selectedChoice.parentElement.classList.add(selectionClass);
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(selectionClass);
-      availableQuestions.splice(currentQuestionIdx, 1);
-
-      if (availableQuestions.length) playGame(availableQuestions);
-      else {
-        alert("Game Over! Score = " + score);
-        window.location.replace("/");
-      }
-    }, 1000);
-  });
-});
-
 let availableQuestions = [...questions];
+let MAX_QUESTIONS = availableQuestions.length;
 let currentQuestion = {};
 let currentQuestionIdx = -1;
 let score = 0;
-acceptingAnswers = true;
+let acceptingAnswers = true;
 playGame(availableQuestions);
