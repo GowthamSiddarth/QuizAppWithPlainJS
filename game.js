a24d3de42e5fcb0e9d1b0c40162031f77c8f4fd4
Gowtham Behara <gothsiddu@gmail.com>
2021-09-03 16:58:56 +0530

    Fetch questions from json file at runtime

--- game.js
+++ game.js
@@ -66,38 +66,17 @@ choicesTextElems.forEach((choiceElem) => {
   });
 });
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3,
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4,
-  },
-];
-
-let availableQuestions = [...questions];
-let MAX_QUESTIONS = availableQuestions.length;
+let availableQuestions = [];
+let MAX_QUESTIONS = 0;
 let currentQuestion = {};
 let currentQuestionIdx = -1;
 let score = 0;
 let acceptingAnswers = true;
-playGame(availableQuestions);
+
+fetch("questions.json")
+  .then((res) => res.json())
+  .then((questions) => {
+    availableQuestions = [...questions];
+    MAX_QUESTIONS = availableQuestions.length;
+    playGame(availableQuestions);
+  });
